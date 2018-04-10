807e76c2bd440bf6a1f579d5d21b5971d519dc4c
Omer Kremer <omer@betterhelp.com>
2018-04-10 00:02:58 -0700

    FB login button

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
-alert('works!');
+function statusChangeCallback(response) {
+    if (response.status == "connected") {
+            // redirect to user bracket
+            console.log("connected");
+            break;
+    }
+}
+
+function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
+};
+
+$(document).ready(function () {
+    FB.Event.subscribe('xfbml.render', function (response) {
+        FB.getLoginStatus(function (response) {
+            statusChangeCallback(response);
+
+        });
+
+    });
+});
