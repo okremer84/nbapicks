a57b804a61dc70166c2014b41610c97f4d73788d
Omer Kremer <omer@betterhelp.com>
2018-04-10 00:59:13 -0700

    Add DB

--- src/index.php
+++ src/index.php
@@ -2,8 +2,10 @@
 
 require_once 'vendor/autoload.php';
 require_once 'lib/Twig.php';
+require_once 'lib/DB.php';
 require_once 'inc/config.php';
 
-// TODO: check login
-// if logged out
 echo Twig::render_template('login.tpl');
+
+// to get DB object
+// $db = DB::get_db();
