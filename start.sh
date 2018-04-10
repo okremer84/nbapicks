33fa072f1e2456a45a49df76cf24d454f9133c2c
Omer Kremer <omer@betterhelp.com>
2018-04-09 21:39:26 -0700

    Basic twig template run

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
-docker run --name ubuntu --rm -p 80:80 -e LOG_STDOUT=true -e LOG_STDERR=true -e LOG_LEVEL=debug -v c:/Users/Omer/Documents/nbapicks/src:/var/www/html fauria/lamp
+SRCDIR = "c:/Users/Omer/Documents/nbapicks/src"
+
+docker pull fauria/lamp
+
+docker run --name ubuntu --rm -p 80:80 -e LOG_STDOUT=true -e LOG_STDERR=true -e LOG_LEVEL=debug -v $SRCDIR:/var/www/html fauria/lamp
