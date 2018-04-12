7821391c4334136aa6ac3e64896f04dd7d29266c
Omer Kremer <omer@betterhelp.com>
2018-04-11 22:44:03 -0700

    Add login verification

--- src/index.php
+++ src/index.php
@@ -1,11 +1,37 @@
 <?php
 
+session_start();
 require_once 'vendor/autoload.php';
 require_once 'lib/Twig.php';
 require_once 'lib/DB.php';
 require_once 'inc/config.php';
 
-echo Twig::render_template('login.tpl');
+$tpl = 'error.tpl';
+$args = [];
+if (empty($_SESSION['email'])) {
+    $tpl = 'login.tpl';
+} elseif (empty($_SESSION['team_name'])) {
+    $args['user']['email'] = $_SESSION['email'];
+    $db = DB::get_db();
+    $stmt = $db->prepare('SELECT team_name FROM user WHERE email = :email');
+    try {
+        $stmt->execute([':email' => $_SESSION['email']]);
+    } catch (PDOException $e) {
+        trigger_error($e->getMessage(), E_USER_WARNING);
+        $tpl = 'error.tpl';
+    }
+    $team_name = $stmt->fetchColumn();
+    if (empty($team_name)) {
+        $tpl = 'submit_team_name.tpl';
+    } else {
+        $tpl = 'bracket.tpl';
+        $_SESSION['team_name'] = $team_name;
+        $args['user']['team_name'] = $team_name;
+    }
+} elseif (!empty($_SESSION['team_name'])) {
+    $args['user']['email'] = $_SESSION['email'];
+    $args['user']['team_name'] = $_SESSION['team_name'];
+    $tpl = 'bracket.tpl';
+}
 
-// to get DB object
-// $db = DB::get_db();
+echo Twig::render_template($tpl, $args);
--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,19 @@
 function statusChangeCallback(response) {
     if (response.status == "connected") {
-            // TODO: Move user to pick team name screen and save email in DB
-            console.log("connected");
-            FB.api('/me?fields=email', function(response) {
-                // send email to API
-                console.log(response.email);
-            });
+        $.ajax({
+            url: "/api/api_handler.php",
+            method: "POST",
+            data: {
+                "fb_id": response.authResponse['userID'],
+                "token": response.authResponse['accessToken'],
+                "login_type": 'fb',
+                "action": 'save_login',
+            }
+        }).done(function(data) {
+            if (data == 'Success') {
+                window.location.replace('/');
+            }
+        });
     }
 }
 
@@ -16,18 +24,19 @@ function checkLoginState() {
 };
 
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    // TODO: Move user to pick team name screen and save email in DB
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
-
-$(window).on("load", function() {
-    FB.Event.subscribe('xfbml.render', function (response) {
-        FB.getLoginStatus(function (response) {
-            statusChangeCallback(response);
-        });
+    var id_token = googleUser.getAuthResponse().id_token;
+    $.ajax({
+        url: "/api/api_handler.php",
+        method: "POST",
+        data: {
+            "token": id_token,
+            "login_type": 'google',
+            "action": 'save_login',
+        }
+    }).done(function(data) {
+        if (data == 'Success') {
+            window.location.replace('/');
+        }
     });
-});
+
+}
